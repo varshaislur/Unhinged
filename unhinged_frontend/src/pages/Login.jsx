82dd0c95aa5dd2840f4e06973c502e804186139f
varshaislur <varshaislur@gmail.com>
2024-07-27 20:23:32 +0530

    completed custom text input

--- unhinged_frontend/src/pages/Login.jsx
+++ unhinged_frontend/src/pages/Login.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TbSocial } from 'react-icons/tb'
 import TextInput from '../components/TextInput'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ 
+    mode: "onChange",
+  });
+  const [errMsg,setErrMsg]=useState("")
+  const [isSubmitting,setIsSubmitting]=useState(false)
+  const dispatch = useDispatch()
+  
+
   return (
     <div className='bg-bgColor h-[100vh] flex items-center justify-center p-6'>
       <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
@@ -16,19 +31,46 @@ const Login = () => {
               Unhinged
             </span>
           </div>
-        </div>
-        <p className='text-ascent-1 text-base font-semibold'>
-        Login to your account
-        </p>
-        <span className='text-sm mt-2 text-ascent-2'>
-Welcome
-        </span>
-        <form className='py-8 flex flex-col gap-5' >
-         <TextInput></TextInput>
-        </form>
+
+          <p className='text-ascent-1 text-base font-semibold'>
+            Login to your account
+          </p>
+          <span className='text-sm mt-2 text-ascent-2'>
+            Welcome
+          </span>
+          <form className='py-8 flex flex-col gap-5' >
+            <TextInput name='email'
+              placeholder='email@example.com'
+              label='Email Address'
+              type='email'
+              register={register("email", {
+                required: "Email Address is required",
+              })}
+              styles='w-full rounded-xl'
+              labelStyle='ml-2'
+              error={errors.email? errors.email.message:""}></TextInput>
+              <TextInput name='password'
+              placeholder='12345678'
+              label='Password'
+              type='password'
+              register={register("password", {
+                required: "Password is required",
+              })}
+              styles='w-full rounded-xl'
+              labelStyle='ml-2'
+              error={errors.email? errors.email.message:""}>
+
+              </TextInput>
+              <Link to="/reset-password" className="text-sm text-right text-blue font semibold" >Forgot Password?</Link>
+              {errMsg?.message && (<span className={`text-sm ${errMsg?.status == "failed"? "text-[#f64949fe]" : "text-[#2ba150fe]"}`}>{errMsg?.message}</span>)}
+              {
+                isSubmitting ? <Loading/> :<CustomButton/>
+              }
+          </form>
         </div>
         {/* Right */}
-      
+
+      </div>
     </div>
   )
 }
